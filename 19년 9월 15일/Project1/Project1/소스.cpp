fbe680e34458c38fcff8baf49da8f2e9aed08ba9
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-10-17 10:41:47 +0900

    ;;;;

--- 19년 9월 15일/Project1/Project1/소스.cpp
+++ 19년 9월 15일/Project1/Project1/소스.cpp
@@ -1,6 +1,7 @@
 #include "global.h"
 int main()
 {
+	cout << pow(2, 3) << std::endl;
 	//std::vector<int> v;
 	//v.push_back(8);
 	//v.push_back(8);
@@ -45,18 +46,18 @@ int main()
 
 	//도전문제
 
-	string dis = "super";
+	//string dis = "super";
 
-	vector<string> words = {"dkdk","dkdk"};
+	//vector<string> words = {"dkdk","dkdk"};
 
-	for (string temp; cin >> temp;) {
-		words.push_back(temp);
-	}
-	sort(words);
+	//for (string temp; cin >> temp;) {
+	//	words.push_back(temp);
+	//}
+	//sort(words);
 
-	for (string d : words) {
-			std::cout << d << std::endl;
-	}
-	cout <<words.begin() << endl;
+	//for (string d : words) {
+	//		std::cout << d << std::endl;
+	//}
+	//cout <<words.begin() << endl;
 
 }
