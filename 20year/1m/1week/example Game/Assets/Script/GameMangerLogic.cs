9305e7e122d3e9a99d727c666e011ca1910b9402
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-12-30 15:20:28 +0900

    3D게임 완성!!
    ㄹㅇㅋㅋ

---
+++ 20year/1m/1week/example Game/Assets/Script/GameMangerLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameMangerLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int totalItemCount; //맵에 존재하는 아이템 총 갯수
+    public int stage;
+    public Text stageCountText;
+    public Text playerCountText;
+
+    private void Awake()
+    {
+        stageCountText.text = "/" + totalItemCount;
+    }
+    public void GetItem(int count)
+    {
+        playerCountText.text = count.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(stage);
+    }
+}
---
+++ 20year/1m/1week/example Game/Assets/Script/PlayerBall.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerBall : MonoBehaviour
+{
+    public int itemCount = 0;
+    public float jumpPower;
+    public GameMangerLogic manager;
+    bool isJump;
+    Rigidbody rigid;
+    AudioSource audio;
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        rigid = GetComponent<Rigidbody>();
+        isJump = false;
+    }
+
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isJump = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            PlayerBall player = other.GetComponent<PlayerBall>();
+            itemCount++;
+            audio.Play();
+            other.gameObject.SetActive(false);
+            manager.GetItem(itemCount);
+        }
+        else if (other.tag == "Point")
+        {
+            if (itemCount == manager.totalItemCount)
+            {
+                if (manager.stage == 1)
+                    SceneManager.LoadScene(0);
+                else
+                    SceneManager.LoadScene((manager.stage + 1));
+                //game Clear!
+            }
+            else
+            {
+                SceneManager.LoadScene(manager.stage);
+                //Restart..
+            }
+
+        }
+    }
+}
