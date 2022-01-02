a47b8cf78e2ef94117979e17bb3c940ae855ae15
SuperHong94 <ghdtnswh213@gmail.com>
2022-01-02 19:24:11 +0900

    Data Binding

---
+++ 21년/DataBinding/DataBinding/Emp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBinding
+{
+    class Emp
+    {
+        public string Ename
+        {
+            get; set;
+
+        }
+
+        public string City
+        {
+            get; set;
+
+        }
+    }
+}
---
+++ 21년/DataBinding/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Emp e = new Emp()
+            {
+                Ename = "홍길동",
+                City = "서울"
+            };
+
+            this.DataContext = e;
+        }
+
+        private void OnClicked(object sender, RoutedEventArgs e)
+        {
+            Emp emp = this.DataContext as Emp;
+            MessageBox.Show(emp.Ename + " " + emp.City);
+        }
+    }
+}
