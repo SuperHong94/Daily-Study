cbccadb7e6521be97cf95f4822340cc765f4c17b
SuperHong94 <ghdtnswh213@gmail.com>
2022-01-27 01:07:15 +0900

    test

--- 21년/Jenkins/JenkinsTest/JenkinsTest/main.cpp
+++ 21년/Jenkins/JenkinsTest/JenkinsTest/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 
 int main()
 {
 	cout << "Hello World!\n";
+
+	char s[] = "if buildOk Test";
+
+	ofstream out{ "test.txt" };
+	if (!out)
+		return 0;
+	out.write(s, sizeof(s));
+	out.close();
 }
